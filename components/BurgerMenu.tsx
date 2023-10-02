9ac12bcc99105a14cb1e781d52e61d16c19e549c
Adilek96 <ashurovadil96@gmail.com>
2023-10-02 23:34:54 +0400

    adding profile icon in burger menu

--- components/BurgerMenu.tsx
+++ components/BurgerMenu.tsx
@@ -1,5 +1,6 @@
 'use client'
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 import { usePathname } from 'next/navigation'
 import {  FaChevronDown, FaSignInAlt,FaWindowClose } from "react-icons/fa";
@@ -36,17 +37,31 @@ export function BurgerMenu() {
         setArticleSubActive(false)
       }
   return (
-        <div className="absolute left-0 top-0 shadow-xl shadow-orange-900 h-[100%] md:w-[200px] sm:w-[100vw] bg-white  rounded-md z-100 overflow-hidden ">
-            <div className="relative">
-                <button className="shadow-xl shadow-primary-800 absolute right-1 top-1 w-[30px] h-[30px]"><FaWindowClose className="absolute right-0 top-1  text-primary-300 w-[30px] h-[30px]"/></button>
-                <button className={`flex h-[100px] px-5  items-center text-md  hover:text-primary-500 duration-300`}><FaSignInAlt className="text-xl mr-2 text-gray-500  font-extrabold"/>Войти</button>
+        <div className="absolute shadow-xl shadow-orange-900 h-[100%] w-[250px]  bg-white  rounded-md z-100 overflow-hidden  ">
+            <div className=" h-[150px] relative flex justify-center items-center gap-2">
+                <button className="shadow-xl shadow-primary-800 absolute right-1 top-0 w-[30px] h-[30px]"><FaWindowClose className="absolute right-0 top-1  text-primary-300 w-[30px] h-[30px]"/></button>
+                <div className="w-[70px] h-[70px] flex items-center justify-center shadow-xl shadow-orange-900 bg-primary-200 rounded-full border-collapse border-[3px] border-primary-500">
+                    <div className="w-[60px] h-[60px] flex items-center justify-center shadow-inner shadow-orange-900 bg-white rounded-full border-collapse border-[2px] border-primary-500">
+        <Image src="/logo/logo.png" 
+          width={60}
+          height={60} 
+          alt="logo"
+        />
+                    </div>
+                </div>
+                <div className="flex flex-col items-center gap-2">
+                    <button className={`flex    items-center text-sm  hover:text-primary-500 duration-300`}><FaSignInAlt className="text-md mr-2 text-gray-500  font-extrabold"/>Войти</button>
+                    <button className="shadow-xl shadow-green-800  w-[100px] flex items-center justify-center text-sm text-center text-white  h-[30px] bg-gradient-to-r from-green-600 to-green-300 rounded-full hover:contrast-125 duration-700  z-10">Гость</button>
+                </div>
+                
             </div>
+            
           <ul>
             <li><Link href="/"   className={` box-content  px-3 flex items-center hover:text-primary-500 duration-300 ${pathname === '/' ? ' text-primary-500 bg-primary-100 h-[70px]' : 'h-[50px]'}`} >Главная</Link></li>
             <li>
                 <Link href="/posts" onMouseEnter ={showPostsSubMenu} onMouseLeave={hidePostsSubMenu} className={` box-content relative   px-3 flex items-center  hover:text-primary-500 duration-500 ${pathname === '/posts' ? ' text-primary-500  bg-primary-100  h-[70px]' : 'h-[50px]'}`} >Обьявления <FaChevronDown  className="text-2xl ml-1 text-gray-400 pt-2 font-extrabold"/> </Link>
                 {postsSubActive ? 
-                <ul onMouseEnter={onMouseInPostsSub}  onMouseLeave={onMouseOutPostsSub} className="sticky shadow-xl shadow-orange-900 h-[40px] w-[200px] bg-white  rounded-b-md z-10  ">
+                <ul onMouseEnter={onMouseInPostsSub}  onMouseLeave={onMouseOutPostsSub} className="sticky shadow-xl shadow-orange-900 h-[40px] w-[250px] bg-white  rounded-b-md z-10  ">
                   <li><Link href="/" className={`h-[40px] px-7 flex items-center  hover:text-primary-500 `}>Продавцы</Link></li>
                 </ul> : ""}
             </li>
@@ -54,7 +69,7 @@ export function BurgerMenu() {
             <li>
                 <Link href="/articles" onMouseEnter ={showArticleSubMenu} onMouseLeave={hideArticleSubMenu} className={`box-content relative  px-3 flex items-center  hover:text-primary-500 duration-500 ${pathname === '/articles' ? ' text-primary-500 bg-primary-100 h-[70px]' : 'h-[50px]'}`}>Статьи <FaChevronDown  className="text-2xl ml-1 text-gray-400 pt-2 font-extrabold"/></Link>
                 {articleSubActive ? 
-                <ul onMouseEnter ={onMouseInArticleSub} onMouseLeave={onMouseOutArticleSub} className="sticky shadow-xl shadow-orange-900 h-[200px] w-[200px] bg-white rounded-b-md z-10 ">
+                <ul onMouseEnter ={onMouseInArticleSub} onMouseLeave={onMouseOutArticleSub} className="sticky shadow-xl shadow-orange-900 h-[200px] w-[250px] bg-white rounded-b-md z-10 ">
                   <li><Link href="/" className={`h-[40px] px-7 flex items-center  hover:text-primary-500 `}>Новости</Link></li>
                   <li><Link href="/" className={`h-[40px] px-7 flex items-center  hover:text-primary-500 `}>Собаки</Link></li>
                   <li><Link href="/" className={`h-[40px] px-7 flex items-center  hover:text-primary-500 `}>Кошки</Link></li>
