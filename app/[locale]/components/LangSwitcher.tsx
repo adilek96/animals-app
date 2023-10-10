ba687327e039d62dd18ab70001f72d02251ac53f
Adilek96 <ashurovadil96@gmail.com>
2023-10-10 23:53:51 +0400

    fix problem on intl

--- app/[locale]/components/LangSwitcher.tsx
+++ app/[locale]/components/LangSwitcher.tsx
@@ -1,14 +1,20 @@
 import React,{useState} from 'react'
 import { IoLanguageSharp } from "react-icons/io5";
 import { FaChevronDown } from "react-icons/fa";
-import Link from 'next/link';
+import Link from 'next-intl/link';
+
 
 
 export  function LangSwitcher() {
-     // стейт вызова дроп-меню языка
+
+  // стейт вызова дроп-меню языка
   const [languageSubActive, setLanguageSubActive] = useState(false)
   // стэйт выбраного языка
   const [choisingLanguage, setChoisingLanguage] = useState("RU")
+
+  
+
+
   return (
     <>
     {/* ----------------------------------- Language change buttons ---------------------------- */}
@@ -18,7 +24,7 @@ export  function LangSwitcher() {
               <button onMouseEnter={() => setLanguageSubActive(true)} onMouseLeave={() => setLanguageSubActive(false)}   className={`box-content relative  px-1 flex items-center  hover:text-primary-500 duration-500`}>{choisingLanguage}<FaChevronDown  className="text-xl ml-1 text-gray-200 pt-1 font-extrabold"/></button>
               {languageSubActive ? 
                 <ul onMouseEnter={() => setLanguageSubActive(true)} onMouseLeave={() => setLanguageSubActive(false)}  className="absolute shadow-lg shadow-orange-900 h-[150px] w-[100px] bg-white rounded-b-md z-10 text-gray-700 " >
-                  <li onClick={() => setChoisingLanguage("AZ")} className={`h-[50px] px-5 flex items-center  hover:text-primary-500 cursor-pointer`}>AZ</li>
+                  <li onClick={() => setChoisingLanguage("AZ")} className={`h-[50px] px-5 flex items-center  hover:text-primary-500 cursor-pointer`}><Link href="/" locale="az">AZ</Link></li>
                   <li onClick={() => setChoisingLanguage("RU")} className={`h-[50px] px-5 flex items-center  hover:text-primary-500 cursor-pointer`}><Link href="/" locale="ru">RU</Link></li>
                   <li onClick={() => setChoisingLanguage("EN")} className={`h-[50px] px-5 flex items-center  hover:text-primary-500 cursor-pointer`}><Link href="/" locale="en">EN</Link></li>
                 </ul> : ""}
