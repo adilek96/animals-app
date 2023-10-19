44f3a6eb7bba077db8f7d5cd4444cca327618153
Adilek96 <ashurovadil96@gmail.com>
2023-10-19 23:41:53 +0400

    test size 2

--- app/[locale]/components/TheBottomMenu.tsx
+++ app/[locale]/components/TheBottomMenu.tsx
@@ -5,12 +5,12 @@ import { FaPlus } from "react-icons/fa";
 
 export  function TheBottomMenu() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-white dark:bg-inherit  dark:text-white  overflow-hidden shadow-md-revers shadow-primary-900 box-content  h-[60px]  text-gray-800 text-[35px]  items-center sm:flex md:hidden  sm:justify-between content-center   px-12 border-t-[5px] border-primary-500 border-solid ">
+    <div className="fixed bottom-0 left-0 right-0 bg-white dark:bg-inherit  dark:text-white  overflow-hidden shadow-md-revers shadow-primary-900 box-content  h-[60px]  text-gray-800 text-[35px]  items-center sm:flex md:hidden  sm:justify-between content-center   px-6 border-t-[5px] border-primary-500 border-solid ">
         <div className=' w-[30vw]  flex justify-around'>
             <BiHome/>
             <BiCategoryAlt/>
         </div>
-        <div className='fixed bottom-[20px] left-[calc(50%-45px)] flex justify-center items-center  w-[90px] h-[90px] bg-gradient-to-r from-green-500 to-green-400 rounded-full hover:contrast-125 duration-700 border-t-[5px]  border-r-[5px] border-l-[5px]  border-primary-500 '><FaPlus className="font-extrabold text-3xl  md:mr-3 text-center"/></div>
+        <div className='fixed bottom-[5px] left-[calc(50%-45px)] flex justify-center items-center  w-[90px] h-[90px] bg-gradient-to-r from-green-500 to-green-400 rounded-full hover:contrast-125 duration-700 border-t-[5px]    border-primary-500 '><FaPlus className="font-extrabold text-3xl  md:mr-3 text-center"/></div>
         <div className=' w-[30vw]  flex justify-around'> 
             <BiMessageRoundedDots/>
             <VscAccount/>
@@ -23,3 +23,4 @@ export  function TheBottomMenu() {
 // VscAccount
 // BiHome
 // BiMessageRoundedDots
+// border-r-[5px] border-l-[5px]
