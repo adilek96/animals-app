8d669a7f5ba6fdb8283b02e6fbcdbaf73eaae95f
Adilek96 <ashurovadil96@gmail.com>
2023-10-19 12:31:22 +0400

    switcher restyled

--- app/[locale]/components/ThemeSwitch.tsx
+++ app/[locale]/components/ThemeSwitch.tsx
@@ -21,16 +21,19 @@ export  function ThemeSwitch({settings}:{settings:boolean}) {
         checked={enabled}
         onChange={setEnabled}
         className={`${enabled ? 'bg-teal-900' : 'bg-teal-700'}
-          relative inline-flex h-[25px] w-[50px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+          relative inline-flex items-center  h-[25px] w-[50px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
       >
         
         <span className="sr-only">Use setting</span>
        
         <span
           aria-hidden="true"
-          className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
-            pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-        />
+          className={`${enabled ? 'translate-x-6' : 'translate-x-[-2px]'}
+            pointer-events-none border-primary-500 border-[2px]  h-[29px] w-[29px] transform rounded-full ${enabled ? "bg-white" : "bg-black" } shadow-lg ring-0 transition duration-200 ease-in-out flex items-center justify-center` }
+        >
+          {enabled ? <RxSun className={`text-yellow-400`}/> : <RxMoon  className={`text-yellow-400`}/>}
+      </span>
+        
       </Switch>
      
 
