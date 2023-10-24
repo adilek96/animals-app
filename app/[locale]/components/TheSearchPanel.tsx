fff7b9ab6da4382f370eb9960805306a7679c5e3
Adilek96 <ashurovadil96@gmail.com>
2023-10-25 00:07:54 +0400

    adding some changes in searchbar

--- app/[locale]/components/TheSearchPanel.tsx
+++ app/[locale]/components/TheSearchPanel.tsx
@@ -9,11 +9,11 @@ import { motion } from "framer-motion";
 export  function TheSearchPanel() {
 
     const icons = [
-        <FaCat size={20} />,
-        <FaDog size={20} />,
-        <FaFish size={20} />,
-        <FaCow size={20} />,
-        <FaKiwiBird size={20} />
+        <FaCat key={1} size={20} />,
+        <FaDog key={2} size={20} />,
+        <FaFish key={3} size={20} />,
+        <FaCow key={4} size={20} />,
+        <FaKiwiBird key={5} size={20} />
       ];
 
    
@@ -43,7 +43,7 @@ export  function TheSearchPanel() {
         </motion.div>
         <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300  bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:outline-none focus:ring dark:focus:ring-primary-500 dark:focus:border-primary-500 rounded-lg" placeholder="Search ..." required />
         {/* <button  type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none  font-medium  text-sm px-4 py-2 bg-gradient-to-r from-green-500 to-green-400 rounded-full hover:contrast-125 duration-700 ">Search</button> */}
-        <SearchButton/>
+        <SearchButton />
     </div>
         </form>
     </div>
--- app/[locale]/components/popUpWindows/categoryWindow/CategoryWindow.tsx
+++ app/[locale]/components/popUpWindows/categoryWindow/CategoryWindow.tsx
@@ -10,7 +10,7 @@ export function CategoryWindow() {
 
 
   return (
-     <motion.div 
+  <motion.div 
     initial={{ opacity: 0, y: -1000 }} 
     animate={{
     opacity: callCategory ? 1 : 0,
