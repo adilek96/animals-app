34f52c560739a284376fae227fcf8f6c1163ea0e
Adilek96 <ashurovadil96@gmail.com>
2023-11-02 13:42:59 +0400

    create sign in page

--- app/[locale]/signIn/page.tsx
+++ app/[locale]/signIn/page.tsx
@@ -14,7 +14,7 @@ export default function SignIn() {
   return (
     <section className=" bg-white dark:bg-gray-700 dark:shadow-gray-800 md:w-[50%] sm:w-[95%] h-[300px] mx-auto my-[30px]  shadow-md  shadow-gray-400 rounded-2xl flex justify-center flex-col items-center">
       <div className="w-[80%] mx-auto flex flex-col justify-center items-center ">
-        {StepOneAnimate()}
+      <h2 className="w-[70%] bg-gray-200 dark:bg-gray-400 shadow-inner font-bold  p-2 rounded-lg  text-[18px]  ">Oooops... Для перехода на эту страницу вы должны быть зарегистрированы!</h2>
       </div>
       <div className="flex  justify-center items-center">
         <button
@@ -27,32 +27,32 @@ export default function SignIn() {
     </section>
   );
 }
-export function StepOneAnimate() {
-  // const t = useTranslations("PostAdding");
-  const text =
-    "Oooops... Для перехода на эту страницу вы должны быть зарегистрированы!";
-  return (
-    <>
-      <motion.h2 className="w-[70%] bg-gray-200 dark:bg-gray-400 shadow-inner font-bold  p-2 rounded-lg  text-[18px]  ">
-        {text.split("").map((char, index) => (
-          <motion.span
-            key={index}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.05, delay: index * 0.1 }}
-          >
-            {char}
-          </motion.span>
-        ))}
-      </motion.h2>
-      {/* <div>
-        <Lottie
-          loop
-          animationData={animations}
-          play
-          style={{ width: 150, height: 150 }}
-        />
-      </div> */}
-    </>
-  );
-}
+// export function StepOneAnimate() {
+//   // const t = useTranslations("PostAdding");
+//   const text =
+//     "Oooops... Для перехода на эту страницу вы должны быть зарегистрированы!";
+//   return (
+//     <>
+//       <motion.h2 className="w-[70%] bg-gray-200 dark:bg-gray-400 shadow-inner font-bold  p-2 rounded-lg  text-[18px]  ">
+//         {text.split("").map((char, index) => (
+//           <motion.span
+//             key={index}
+//             initial={{ opacity: 0 }}
+//             animate={{ opacity: 1 }}
+//             transition={{ duration: 0.05, delay: index * 0.1 }}
+//           >
+//             {char}
+//           </motion.span>
+//         ))}
+//       </motion.h2>
+//       {/* <div>
+//         <Lottie
+//           loop
+//           animationData={animations}
+//           play
+//           style={{ width: 150, height: 150 }}
+//         />
+//       </div> */}
+//     </>
+//   );
+// }
